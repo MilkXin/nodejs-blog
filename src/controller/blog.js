9af0ed25a962c16c5d09180c5cef93b9374d9d50
chen xin <729609320@.com>
2019-10-01 23:01:40 +0800

    连接数据库

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -1,21 +1,20 @@
+const { exec } = require('../db/mysql')
+
 const getList = (author, keyword) => {
-    //先返回假数据，格式正确
-    return [
-        {
-            id: 1,
-            title: '标题1',
-            content: '内容1',
-            createTime: 1569840523026,
-            author: 'zhangsan'
-        },
-        {
-            id: 2,
-            title: '标题2',
-            content: '内容2',
-            createTime: 1569840615335,
-            author: 'lisi'
-        }
-    ]
+    let sql = `select * from blogs where 1=1 `
+
+    if (author) {
+        sql += `and author='${author}' `
+    }
+
+    if (keyword) {
+        sql += `and title like '%${keyword}%' `
+    }
+
+    sql += `order by createtime desc;`
+
+    // 返回 promise
+    return exec(sql)
 }
 
 const getDetail = (author, keyword) => {
