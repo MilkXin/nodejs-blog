87bcac10f4788afb05c202caf79ee16d0c644251
chen xin <729609320@.com>
2019-11-30 21:51:05 +0800

    操作cookie

--- src/router/user.js
+++ src/router/user.js
@@ -1,6 +1,12 @@
 const { login} = require('../controller/user')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
+//获取 cookie的过期时间
+const getCookieExpires = () => {
+    const d = new Date()
+    d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+    return d.toGMTString()
+}
 const handleUserRouter = (req, res) => {
     const method = req.method // GET POST
 
@@ -10,6 +16,8 @@ const handleUserRouter = (req, res) => {
         const result = login(username, password)
         return result.then(data => {
             if (data.username) {
+                //操作 cookie
+                res.setHeader('Set-Cookie', `username=${username}; path=/; httpOnly; expires=${getCookieExpires()}`)
                 return new SuccessModel()
             }
 
